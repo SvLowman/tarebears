9d976ddfae99fb49e3dbaaef2c05b72d21529ae3
SvLowman <SvenLohmann@gmx.de>
2021-02-28 09:58:18 +0100

    :construction: Build basic structure for valuedisplay

--- src/pages/ValuePage.js
+++ src/pages/ValuePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import ValueForm from "../components/ValueForm";
+import ValueDisplay from "../components/ValueDisplay";
 import Footer from "../components/Footer";
 
 function ValuePage() {
@@ -9,6 +10,7 @@ function ValuePage() {
       <Header />
       <p>😀</p>
       <ValueForm />
+      <ValueDisplay />
       <Footer />
     </>
   );
