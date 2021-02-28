9688ed0301d99e6f77a4161389b6ae4ecf13e7bc
SvLowman <SvenLohmann@gmx.de>
2021-02-28 09:58:18 +0100

    :construction: Build basic structure for valuedisplay

---
+++ src/components/ValueDisplay.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./valuedisplay.css";
+
+function ValueDisplay() {
+  return (
+    <>
+      <div className="valueDisplayContainer">Liste der bestehenden Werte:</div>
+    </>
+  );
+}
+
+export default ValueDisplay;
