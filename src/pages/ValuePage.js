ea62986a3fd377799125b0c8309df4ffe1cd97cd
SvLowman <SvenLohmann@gmx.de>
2021-02-28 09:43:56 +0100

    :construction: Build basic structure of value form

--- src/pages/ValuePage.js
+++ src/pages/ValuePage.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "../components/Header";
+import ValueForm from "../components/ValueForm";
+import Footer from "../components/Footer";
 
 function ValuePage() {
   return (
     <>
+      <Header />
       <p>😀</p>
+      <ValueForm />
+      <Footer />
     </>
   );
 }
