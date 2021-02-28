dc83e770b5fb461fd7b6471788e67fd226449cac
SvLowman <SvenLohmann@gmx.de>
2021-02-28 14:30:32 +0100

    :sparkles: Make possible to display stored data on value display

--- src/components/ValueDisplay.js
+++ src/components/ValueDisplay.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./valuedisplay.css";
 
-function ValueDisplay() {
+function ValueDisplay({ valueArray }) {
+  console.log(valueArray);
   return (
     <>
-      <div className="valueDisplayContainer">Liste der bestehenden Werte:</div>
+      <div className="valueDisplayContainer">
+        <h3>Liste der bestehenden Werte:</h3>
+        <div>
+          {valueArray &&
+            valueArray.map((value, index) => (
+              <p key={index}>
+                {value.foodType} : {value.contentValue} g / 100g
+              </p>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/ValuePage.js
+++ src/pages/ValuePage.js
@@ -1,16 +1,17 @@
-import React from "react";
+import { React, useState } from "react";
 import Header from "../components/Header";
 import ValueForm from "../components/ValueForm";
 import ValueDisplay from "../components/ValueDisplay";
 import Footer from "../components/Footer";
 
 function ValuePage() {
+  const [valueArray, setValueArray] = useState([]);
   return (
     <>
       <Header />
       <p>😀</p>
-      <ValueForm />
-      <ValueDisplay />
+      <ValueForm valueArray={valueArray} setValueArray={setValueArray} />
+      <ValueDisplay valueArray={valueArray} />
       <Footer />
     </>
   );
