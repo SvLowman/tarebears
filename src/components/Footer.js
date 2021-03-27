3852085ceb094335e64647418f502caa94c098d9
SvLowman <SvenLohmann@gmx.de>
2021-03-27 23:52:01 +0100

    :art: Implement functional footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./footer.css";
 
 function Footer() {
+  const location = useLocation();
   return (
     <>
       <footer className="footer">
-        <div className="footerText">Der Footer.</div>
+        <Link to="/">
+          <div className="footerText" active={location.pathname === "/"}>
+            Rechner
+          </div>
+        </Link>
+        <Link to="/valuepage">
+          <div
+            className="footerText"
+            active={location.pathname === "/valuepage"}
+          >
+            Meine Werte
+          </div>
+        </Link>
+        <Link to="/about">
+          <div className="footerText" active={location.pathname === "/about"}>
+            Über TareBears
+          </div>
+        </Link>
       </footer>
     </>
   );
